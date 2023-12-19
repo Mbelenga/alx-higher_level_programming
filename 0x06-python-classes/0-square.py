d5e0684960050e6b11fbb66a6545ce0138d8e24b
Mbelenga <mbelengaterence@gmail.com>
2023-12-19 01:42:18 -0800

    an empty class Square that defines a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
+
 """an empty class called square"""
 
-class square:
-  """The class is empty it does nothing"""
-  
 
-  pass
+class square:
+    """The class is empty it does nothing"""
+
+    pass
