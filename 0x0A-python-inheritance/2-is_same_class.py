53cb8a77ea8a00f1573b20612bbbec4455f125cb
Mbelenga <mbelengaterence@gmail.com>
2024-01-08 12:06:43 -0800

    problem

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Returns an instance of a class"""
+
+def is_same_class(obj, a_class):
+    """ Defined function """
+    if type(obj) == a_class:
+        return (true)
+    else:
+        return (false)
