2e13e78bd83b9cc107e69a723460c28410b5714e
Mbelenga <mbelengaterence@gmail.com>
2024-01-09 04:52:12 -0800

    problem

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,7 +6,5 @@ def read_file(filename=""):
 
     """This function reads a text file"""
 
-    with open (filename, encoding='utf-8') as file:
-
-        content = file.read()
-    print(content)
+    with open (filename, encoding="utf-8") as file_content:
+        print(file_content.read(), end='')
