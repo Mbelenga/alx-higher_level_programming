f41dc471692f62ee28b9097c1b6f858844ef52d7
Mbelenga <mbelengaterence@gmail.com>
2024-01-09 06:01:10 -0800

    a function that returns the JSON representation of an object

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -7,7 +7,7 @@ import json
 
 def to_json_string(my_obj):
     """
-    A function that returns the JSON 
+    A function that returns the JSON
     representation of an object
     """
     return json.dumps(my_obj)
