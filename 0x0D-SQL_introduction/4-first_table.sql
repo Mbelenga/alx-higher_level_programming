04e16db4d324aabe810880d15ab2d7c6b1f8cbd2
Mbelenga <mbelengaterence@gmail.com>
2024-03-07 01:54:16 -0800

    to be updated

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,4 @@
--- creates a table called first_table in the current DB in my MySQL server
--- If the table first_table already exists, your script should not fail
+-- A script that creates a table called first_table
 -- You are not allowed to use the SELECT or SHOW statements
 -- attributes include id and name
 
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- A script that prints the full description of the table first_table
+-- The database name will be passed as an argument of the mysql command
+-- Not allowed to use the DESCRIBE or EXPLAIN statements
