5dfa8eb26e98cfa3151dde04dc09d568cf7dde61
Mbelenga <mbelengaterence@gmail.com>
2024-03-07 04:28:24 -0800

    A script that updates the score of Bob to 10

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -2,6 +2,7 @@
 -- You are not allowed to use Bob's id value, only the name field
 -- The database name will be passed as an argument of the mysql command
 
-SELECT name
-FROM second_table
-UPDATE 10
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
+
