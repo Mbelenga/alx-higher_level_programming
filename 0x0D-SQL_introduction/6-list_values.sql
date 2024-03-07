6dd11d1411dc0294537838bd4381004d8689c078
Mbelenga <mbelengaterence@gmail.com>
2024-03-07 02:09:02 -0800

    a script that lists all rows of the table first_table

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,1 +1,4 @@
 -- A script that list all rows of the table first_table
+-- All fields should be printed
+-- The database name will be passed as an argument of the mysql command
+SELECT * FROM first_table
