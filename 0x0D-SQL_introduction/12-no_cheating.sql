c5b3aab4c7364fae7300d93b8a6c0f6ae893d85a
Mbelenga <mbelengaterence@gmail.com>
2024-03-07 04:22:52 -0800

    corrections

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,7 @@
+-- A script that updates the score of Bob to 10 in the second_table
+-- You are not allowed to use Bob's id value, only the name field
+-- The database name will be passed as an argument of the mysql command
+
+SELECT name
+FROM second_table
+UPDATE 10
