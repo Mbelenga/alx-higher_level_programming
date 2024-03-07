6ac7d7b97797297780d2864c9f44eff1f79b4777
Mbelenga <mbelengaterence@gmail.com>
2024-03-07 02:24:24 -0800

    A script that inserts a new row in the table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- A script that inserts a new row in the table first_table
 -- The new row has id = 89, name = Best School
 INSERT INTO first_table(id,name)
-VALUES(89,Best School);
+VALUES(89, 'Best School');
