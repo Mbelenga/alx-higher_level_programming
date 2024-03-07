2db4dab8189e62046d2c8feb650bd7e51ea9e6de
Mbelenga <mbelengaterence@gmail.com>
2024-03-07 05:01:50 -0800

    to be updated

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- A script that computes the score average of all records in the second_table
+-- Result column name should be average
+-- The database name will be passed as an argument of the mysql command
