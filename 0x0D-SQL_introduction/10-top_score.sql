6db16d734cc8ec0c47277a849950451bda5a7735
Mbelenga <mbelengaterence@gmail.com>
2024-03-07 03:55:46 -0800

    to be updated

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- A script that lists all records of the second_table
+-- Results should display both the score and the name
+-- Records should be ordered by score(top first)
+-- The database name will be passed as an argument of the mysql command
