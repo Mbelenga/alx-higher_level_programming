84b644bd3e5cdbb39783cd90c91f6ee68424f806
Mbelenga <mbelengaterence@gmail.com>
2024-03-11 08:38:13 -0700

    A script that prints x times 'C is fun'

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,9 @@
 
 const arg = parseInt(process.argv[2]);
 if (!arg) {
-	console.log('Missing number of occurences')
+  console.log('Missing number of occurences');
 } else {
-	for (let i = 0; i < arg; i++) {
-		console.log('C is fun');
-	}
+  for (let i = 0; i < arg; i++) {
+    console.log('C is fun');
+  }
 }
