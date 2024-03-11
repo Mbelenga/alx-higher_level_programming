77ccfbe2b2dfe8edc95d91ea86e6064c1197494d
Mbelenga <mbelengaterence@gmail.com>
2024-03-11 08:14:39 -0700

    A script that prints 3 lines but by using an array of string and a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const myArr = [
-	`C is fun`,
-	`Python is cool`,
-	`Javascript is amazing`
+  'C is fun',
+  'Python is cool',
+  'Javascript is amazing'
 ];
 for (let i = 0; i < myArr.length; i++) {
   console.log(myArr[i]);
