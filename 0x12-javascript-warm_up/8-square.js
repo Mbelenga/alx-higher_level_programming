eed890cb78060dfdaf0b63856f995086625ea43f
Mbelenga <mbelengaterence@gmail.com>
2024-03-11 14:37:20 -0700

    A script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const size = parseInt(process.argv[2]);
+if (!size) {
+  console.log('Missing size');
+} else {
+  for (let index = 0; i < size; index++) {
+    console.log('X'.repeat(size));
+  }
+}
