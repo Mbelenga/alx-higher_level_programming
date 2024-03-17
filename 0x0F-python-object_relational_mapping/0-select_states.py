9accc1c876dd27aaa92d7cc9fb18c178266c0efc
Mbelenga <mbelengaterence@gmail.com>
2024-03-17 08:30:41 -0700

    a script that lists all states from the database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,23 +8,22 @@ from sys import argv
 if __name__ == "__main__":
 
 # Connect to the database
-my_db = MySQLdb.connect(host='localhost', user=argv[1], password=argv[2],
-                            db=argv[3], port=3306)
-    # Create cursor obj to interact with database
-    my_cursor = my_db.cursor()
+my_db = MySQLdb.connect(host='localhost', user=argv[1], password=argv[2],db=argv[3], port=3306)
+# Create cursor obj to interact with database
+my_cursor = my_db.cursor()
 
-    # Execute a SELECT query to fetch data
-    my_cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
+# Execute a SELECT query to fetch data
+my_cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
 
-    # fetch all the data returned by the query
-    my_data = my_cursor.fetchall()
+# fetch all the data returned by the query
+my_data = my_cursor.fetchall()
 
-    # Iterate through the fetched data and print each row
-    for row in my_data:
-        print(row)
+# Iterate through the fetched data and print each row
+for row in my_data:
+print(row)
 
-    # Close all cursors
-    my_cursor.close()
+# Close all cursors
+my_cursor.close()
 
-    # Close all databases
-    my_db.close()
+# Close all databases
+my_db.close()
