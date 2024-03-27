4c9862affc70baaa1da1faeb120a54aa1dba7999
Mbelenga <mbelengaterence@gmail.com>
2024-03-27 15:10:46 -0700

    a Bash script that takes in a URL, sends a GET request to the URL

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-
+# Get request to the URL and display the body of the response
 curl -sL "$1"
-=======
-# A script that takes in an URL and sends a GET request to it.
-curl -s "$1" | wc -c
->>>>>>> c10702096334b2723f01098d134827893610159d
