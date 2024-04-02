04e4ffa1aa69b209461d18fcdde558e06d33adaf
Mbelenga <mbelengaterence@gmail.com>
2024-04-02 02:46:02 -0700

    Response header value

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" A script that fetches the given URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header """
+""" A script that fetches the given URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header"""
 
 import requests
-import sys
+from sys import argv
 
 if __name__ == "__main__":
 
